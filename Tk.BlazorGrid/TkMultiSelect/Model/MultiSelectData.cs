ccb9b31f31c550cb708823a6cda55191bbf4cb8e
Ganesh G <gharmalkar.ganesh@gmail.com>
2020-06-09 11:42:51 -0500

    functioning multiselect push

--- Tk.BlazorGrid/TkMultiSelect/Model/MultiSelectData.cs
+++ Tk.BlazorGrid/TkMultiSelect/Model/MultiSelectData.cs
@@ -7,15 +7,21 @@ namespace TkMultiSelect
 
     public class MultiSelectItem
     {
-        public MultiSelectItem(string id, string text)
+        public MultiSelectItem(string id, string text, bool isSelected = false, string dbKeyId = null)
         {
             Id = id;
             Text = text;
+            IsSelected = isSelected;
+            DbKeyId = dbKeyId;
         }
 
         public string Id { get; set; }
 
         public string Text { get; set; }
+
+        public bool IsSelected { get; set; }
+
+        public string DbKeyId { get; set; }
     }
 
     public class MultiSelectItems
@@ -28,13 +34,13 @@ namespace TkMultiSelect
         public List<MultiSelectItem> Data { get; }
     }
 
-    public class MultiSelectItemsSelected
-    {
-        public MultiSelectItemsSelected()
-        {
-            Data = new List<MultiSelectItem>();
-        }
+    //public class MultiSelectItemsSelected
+    //{
+    //    public MultiSelectItemsSelected()
+    //    {
+    //        Data = new List<MultiSelectItem>();
+    //    }
 
-        public List<MultiSelectItem> Data { get; }
-    }   
+    //    public List<MultiSelectItem> Data { get; }
+    //}   
 }
